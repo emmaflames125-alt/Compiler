bd8e0147085e0f65b0b3bca6b85b81b8e3e78070
emmaflames125-alt <emmaflames125@gmail.com>
2026-03-29 17:26:51 +0100

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-const CACHE_NAME = 'rop-compiler-v1';
+const CACHE_NAME = 'rop-compiler-v2';   // change version number if you update files later
+
 const urlsToCache = [
-  './',
-  './index.html',
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())   // activate immediately
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
